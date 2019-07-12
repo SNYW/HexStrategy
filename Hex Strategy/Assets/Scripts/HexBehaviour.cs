6365f27eb0a623341b67ae5a9db02f4d4f081c11
Sonny Williams <sonnywilliams@hotmail.co.uk>
2019-07-12 16:28:10 +0100

    Initial commit

---
+++ Hex Strategy/Assets/Scripts/HexBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.log("Alive");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
